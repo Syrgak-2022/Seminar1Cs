1e78d6e28656fe22b44fae0315b506d5d3955ee8
Сыргак Иманкулов <syrgakimankulov@gmail.com>
2022-08-09 03:04:36 +0300

    Add 3 work

--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
-﻿Console.Write("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+﻿// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if(firstNumber == secondNumber * secondNumber)
+// if(firstNumber == secondNumber * secondNumber)
+// {
+//     Console.WriteLine(firstNumber + " является квадратом от " + secondNumber);
+// }
+// else
+// {
+//     Console.WriteLine(firstNumber + " НЕ является квадратом от " + secondNumber);
+// }
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int negativeNumber = number * (-1);
+
+while (negativeNumber <= number)
 {
-    Console.WriteLine(firstNumber + " является квадратом от " + secondNumber);
-}
-else
-{
-    Console.WriteLine(firstNumber + " НЕ является квадратом от " + secondNumber);
+    Console.WriteLine(negativeNumber);
+    negativeNumber = negativeNumber + 1;
 }
